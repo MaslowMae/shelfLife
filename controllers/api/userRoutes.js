137b6e09bc509099ecd088a57e39ba9c738809a5
MaslowMae <bmm10025@gmail.com>
2024-04-03 19:32:35 -0400

    update user routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,4 +1,8 @@
 const router = require('express').Router();
 const {User} = require('./models');
 
-router.post('./login',async)
+router.post('./login',async (req,res)=>{
+    try {
+        const userData= await user.findOne({ where:})
+    }
+
