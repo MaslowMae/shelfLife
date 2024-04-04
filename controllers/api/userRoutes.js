ea899b07fc0c73902612fb4bf871d4b8a7ee45d5
MaslowMae <bmm10025@gmail.com>
2024-04-03 20:31:08 -0400

    added homeroutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,12 +1,45 @@
 const router = require('express').Router();
-const {User} = require('../../models');
+const { User } = require('../../models');
 
-// router.post('./login',async (req,res)=>{
-//     try {
-//         const userData= await user.findOne({ where:{email: req.body.email}});
-//         if(!userData) {
-//             res
-//             .status(400)
-//         }
-//     }
 
+// log in route
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { email: req.body.email } });
+        if (!userData) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect email or password, please try again' });
+            return;
+        }
+//   checking for valid password
+        const validPassword = await userData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect email or password, please try again' });
+            return;
+        }
+// session save for status of log in 
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            res.json({ user: userData, message: 'You are now logged in!' });
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+//logout route
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+module.exports = router;
