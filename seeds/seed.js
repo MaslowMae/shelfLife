4da94a84849f4136c5faa06b9e8fa8df3b2baca5
Bradley Troll <bradleytroll@gmail.com>
2024-04-04 18:55:30 -0400

    Seeds

---
+++ seeds/seed.js
@@ -0,0 +1,26 @@
+const sequelize = require('../config/connection');
+const { User, Book, Post, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const bookData = require('./bookData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+    
+    await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+    
+    await Book.bulkCreate(bookData);
+    
+    await Post.bulkCreate(postData);
+    
+    await Comment.bulkCreate(commentData);
+    
+    process.exit(0);
+
+};
+seedDatabase();
