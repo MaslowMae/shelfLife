be6007137e7d6602e192c503f4017261a77599c7
Bradley Troll <bradleytroll@gmail.com>
2024-04-07 18:52:23 -0400

    Basic books.handlebars file for search results.

--- seeds/seed.js
+++ seeds/seed.js
@@ -8,24 +8,10 @@ const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
-<<<<<<< HEAD
-    
-=======
-
->>>>>>> main
     await User.bulkCreate(userData, {
         individualHooks: true,
         returning: true,
     });
-<<<<<<< HEAD
-    
-    await Book.bulkCreate(bookData);
-    
-    await Post.bulkCreate(postData);
-    
-    await Comment.bulkCreate(commentData);
-    
-=======
 
     await Book.bulkCreate(bookData);
 
@@ -33,7 +19,6 @@ const seedDatabase = async () => {
 
     await Comment.bulkCreate(commentData);
 
->>>>>>> main
     process.exit(0);
 
 };
