94738e09746e2cb08c6037d5f33de41d4202252b
Ericlkm <gtprinceeric.el1@gmail.com>
2024-04-11 17:57:10 -0400

    made routes and signup page

---
+++ controllers/api/posts.js
@@ -0,0 +1,49 @@
+const router = require("express").Router();
+const { Post, User } = require("../../models");
+const withAuth = require("../../utils/auth");
+
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      attributes: ["postTitle", "post_content"],
+      include: [{ model: User, attributes: ["username"] }],
+    });
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+
+  res.render("post");
+});
+
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+    if (!postData) {
+      res.status(404).json({ message: "No post found with this id!" });
+      return;
+    }
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
